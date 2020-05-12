26a218eecd19b5ee3c7377d1c5da8897f34712d9
Edgard Massot <emassot@gmail.com>
2020-05-13 00:40:43 +0200

    Session 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React,{useState,useEffect} from 'react';
-import './App.css';
 import {hot} from 'react-hot-loader';
 import firebase from 'firebase';
 import {firestore} from 'firebase';
 
-// Initialize Cloud Firestore through Firebase
+import {getMonday,formatDate} from './menu_common';
 
+// Initialize Cloud Firestore through Firebase
 if (!firebase.apps.length) {
   firebase.initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
@@ -14,20 +14,24 @@ if (!firebase.apps.length) {
   });
 }
 
+const uid = 'user';
+
+const week = [
+  {day:'Lunes',key:'monday'},
+  {day:'Martes',key:'tuesday'},
+  {day:'Miércoles',key:'wednesday'},
+  {day:'Jueves',key:'thursday'},
+  {day:'Viernes',key:'friday'},
+  {day:'Sábado',key:'saturday'},
+  {day:'Domingo',key:'sunday'}
+];
+
+
 var db = firebase.firestore();
 
 function App() {
   const [isAdding,toggleAdding] = useState(false);
-  const [menu,setMenu] = useState([
-    {day:'Lunes',key:'monday',menu:[]},
-    {day:'Martes',key:'tuesday',menu:[]},
-    {day:'Miércoles',key:'wednesday',menu:[]},
-    {day:'Jueves',key:'thursday',menu:[]},
-    {day:'Viernes',key:'friday',menu:[]},
-    {day:'Sábado',key:'saturday',menu:[]},
-    {day:'Domingo',key:'sunday',menu:[]}
-  ]); 
-  
+
   const [currentMenu,setCurrentMenu] = useState(null);
 
   const [meals,setMeals] = useState([])
@@ -45,15 +49,17 @@ function App() {
       setMeals(data);
     });
 
-    db.collection("menus").get().then((querySnapshot) => {
-      let data = querySnapshot.docs.map(doc=>{
+    const key = `${uid}${formatDate(getMonday())}`;
+
+   
+    let doc = db.collection("menus").doc(key).get().then(doc=>{
+
+      if (doc.exists) {
         let d = doc.data();
         d.id = doc.id;
         setCurrentMenu(d);
-        return d;
-      });
+      } else {
 
-      if (data.length == 0) {
         let newmenu = {
           date:new Date(),
           monday:[],
@@ -64,38 +70,29 @@ function App() {
           saturday:[],
           sunday:[],
         };
-
-        db.collection("menus").add(newmenu)
-        .then(function(docRef) {
-          console.log("Document written with ID: ", docRef.id);
-          setCurrentMenu(docRef);
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });
+        db.collection("menus").doc(key).set(newmenu);
+        setCurrentMenu(newmenu);
       }
-
-
     });
-
-
   },[])
+
+
+  const toggleadd = (value)=> {
+    changeMeal('');
+    toggleAdding(value == undefined  ? !isAdding : value);
+
+  }
 
   
   const addToDay = (e,day,dish) => {
     e.preventDefault();
-    currentMenu[menu[day].key].push(dish);
-    menu[day]['menu'].push(dish);
-    setMenu([...menu]);
-    toggleAdding(false);  
+    currentMenu[week[day].key].push(dish);
+    toggleadd(false);  
 
     let themenu = db.collection("menus").doc(currentMenu.id);
-    // Atomically add a new region to the "regions" array field.
     themenu.update({
-        [menu[day].key]: firebase.firestore.FieldValue.arrayUnion(dish)
+        [week[day].key]: firebase.firestore.FieldValue.arrayUnion(dish)
     });
-
-
   }
 
   const createMeal = ()=>{
@@ -105,51 +102,56 @@ function App() {
     })
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
-        setMeals([...meals,{id:docRef.if,name:meal}]);
+        setMeals([...meals,{id:docRef.id,name:meal}]);
     })
     .catch(function(error) {
         console.error("Error adding document: ", error);
     });
-
-
-    
     changeMeal('');
   }
 
-  const filteredmeals = meal.name != '' ? 
+  const filteredmeals = meal != '' ? 
     meals.filter(m=>{
-      let r = RegExp(meal.name,'i');
-      return r.test(m);
+      let r = RegExp(meal,'i');
+      return r.test(m.name);
     }) : 
     meals;
 
-  if ( isAdding !== false )
-  return (
-    <div>
-      <form onSubmit={e=>{e.preventDefault(); createMeal()}}> 
-        <input autoComplete="off" onChange={(e)=>changeMeal(e.target.value)} placeholder="Escribe para filtrar o crear"/>
-        {filteredmeals.map((meal,i) => (
-          <div key={i}><a href="" onClick={e=>addToDay(e,isAdding,meal)}>{meal.name}</a></div>
-        ))}
-        <div><button>Crear nuevo plato</button></div>
-      </form>
-      <button onClick={()=>toggleAdding(false)}>Cerrar</button>
-    </div>
-  )
 
   return (
-    <div className="App">
-      {menu.map( (day,i) => {
+    <div className="p-2 flex flex-col items-center">
+      {isAdding !== false && (
+        <div className="fixed inset-0 w-full items-center flex justify-center" style={{backgroundColor:'rgba(0,0,0,0.5)'}}>
+          <div className="bg-white w-3/4 p-2 rounded" style={{height: "75vh"}}>
+            <button className="float-right bg-red-600 text-white font-bold p-1 rounded" onClick={()=>toggleadd(false)}>X</button>
+            <form onSubmit={e=>{e.preventDefault(); createMeal()}}> 
+              <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-2 block w-3/4 appearance-none leading-normal"  autoComplete="off" onChange={(e)=>changeMeal(e.target.value)} placeholder="Escribe para filtrar o crear"/>
+              {filteredmeals.map((meal,i) => (
+                <div key={i}><a href="" onClick={e=>addToDay(e,isAdding,meal)}>{meal.name}</a></div>
+              ))}
+              <div><button>Crear nuevo plato</button></div>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {week.map( (day,i) => {
 
         return(
-          <div key={i}>
-            <h2>{day.day}</h2>
-            {currentMenu != null && currentMenu[day.key].map( (dish, j) => (
-              <p key={j}>
-                {dish.name}
-              </p>
-            ))}
-            <button onClick={()=>toggleAdding(i)}>Añadir</button>
+          <div key={i} className="mb-2 flex flex-col w-3/4">
+            <h2 className="text-2xl underline">{day.day}</h2>
+            <div className="flex justify-between">
+              <div className="">
+                {currentMenu != null && currentMenu[day.key].map( (dish, j) => (
+                  <p key={j} className="pl-4">
+                    {dish.name}
+                  </p>
+                ))}
+              </div>
+              <div>
+                <button className="bg-blue-500 py-1 px-2 rounded text-white hover:bg-blue-600" onClick={()=>toggleadd(i)}>Añadir</button>
+              </div>
+            </div>
           </div>
         )
 
