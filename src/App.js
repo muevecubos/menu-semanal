0b1dd99a9e0c62e73c9952e2e7e91d415e4add49
Edgard Massot <emassot@gmail.com>
2020-05-19 01:12:16 +0200

    Sesion 7

--- src/App.js
+++ src/App.js
@@ -48,8 +48,8 @@ function App() {
 
   const [meal,changeMeal] = useState('');
   const [meal_type,setMealType] = useState('lunch');
-
   const [show_day,setShowDay] = useState(false);
+  const [day_number,setDayNumber] = useState(null);
 
   useEffect(()=>{
 
@@ -102,14 +102,14 @@ function App() {
 
   }
 
-  const viewDay = (e,day)=>{
-    if (e != undefined) e.preventDefault();
+  const viewDay = (day,week_day)=>{
     setShowDay(day);
+    setDayNumber(week_day);
   }
 
   
   const addToDay = (e,day_key,dish) => {
-    e.preventDefault();
+    if (e != undefined)e.preventDefault();
 
     if (currentMenu[day_key][meal_type] == undefined) currentMenu[day_key][meal_type] = [];
     currentMenu[day_key][meal_type].push(dish);
@@ -165,46 +165,47 @@ function App() {
     const day_name = week.find(d=>d.key == show_day);
 
     return (
-      <div className="flex flex-col w-3/4 mx-16 py-4">
+      <div className="bg-blue-100 w-full" style={{height:'100vh'}}>
+        <div className="flex flex-col w-3/4 mx-16 py-4 ">
 
-      <div className="flex border-b-4 border-gray-300 pb-2">
-        <div className="border border-black w-8 text-center font-bold text-xl" style={{borderTop:"5px solid red"}}>
-          11
-        </div>
-        <div className="text-2xl flex justify-center flex-grow">
-          {day_name.day}
-        </div>
-      </div>
-
-
-
-      {isAdding !== false && (
-        <div className="fixed inset-0 w-full items-end flex " style={{backgroundColor:'rgba(0,0,0,0.5)'}}>
-          <div className="bg-white w-full p-2 " style={{height: "65vh"}}>
-            <button className="float-right bg-red-600 text-white font-bold p-1 rounded" onClick={()=>toggleadd(false)}>X</button>
-            <form className="flex flex-col items-center" onSubmit={e=>{e.preventDefault(); createMeal()}}> 
-              <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-2 block w-3/4 appearance-none leading-normal"  autoComplete="off" onChange={(e)=>changeMeal(e.target.value)} placeholder="Escribe para filtrar o crear"/>
-              <div className="w-3/4 overflow-y-scroll" style={{height:"50vh"}}>
-              {filteredmeals.map((meal,i) => (
-                <div key={i} className="border border-yellow-500 bg-orange-200 p-2 m-1">
-                  <a href="" onClick={e=>addToDay(e,isAdding,meal)}>{meal.name}</a>
-                </div>
-              ))}
-              </div>
-              <div className="flex justify-end">
-                <button className="bg-blue-500 py-1 px-2 rounded text-white hover:bg-blue-600">Crear nuevo plato</button>
-              </div>
-            </form>
+        <div className="flex border-b-4 border-gray-300 pb-2">
+          <div className="border border-black w-8 text-center font-bold text-xl" style={{borderTop:"5px solid red"}}>
+            {day_number}
+          </div>
+          <div className="text-2xl flex justify-center flex-grow">
+            {day_name.day}
           </div>
         </div>
-      )}
+
+
+
+        {isAdding !== false && (
+          <div className="fixed inset-0 w-full items-end flex " style={{backgroundColor:'rgba(0,0,0,0.5)'}}>
+            <div className="bg-blue-100 w-full p-2 " style={{height: "65vh"}}>
+              <button className="float-right bg-red-600 text-white font-bold p-1 rounded" onClick={()=>toggleadd(false)}>X</button>
+              <form className="flex flex-col items-center" onSubmit={e=>{e.preventDefault(); createMeal()}}> 
+                <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-2 block w-3/4 appearance-none leading-normal"  autoComplete="off" onChange={(e)=>changeMeal(e.target.value)} placeholder="Escribe para filtrar o crear"/>
+                <div className="w-3/4 overflow-y-scroll" style={{height:"50vh"}}>
+                {filteredmeals.map((meal,i) => (
+                  <div key={i} className="border border-blue-500 bg-blue-200 p-2 m-1 cursor-pointer" onClick={()=>addToDay(undefined,isAdding,meal)}>
+                    {meal.name}
+                  </div>
+                ))}
+                </div>
+                <div className="flex justify-end">
+                  <button className="bg-blue-500 py-1 px-2 rounded text-white hover:bg-blue-600">Crear nuevo plato</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
 
         {currentMenu != null && lunch_time.map((lt,i)=>{
           const meals = day_showing[lt.key] || [];
           return (
-            <div className="border-b-2 border-blue-200 pb-4 mb-4">
+            <div className="border-2 border-blue-500 bg-blue-300 rounded p-2 mb-2 shadow-xl">
 
-              <div className="flex justify-center cursor-pointer hover:bg-blue-100 rounded" onClick={()=>toggleadd(show_day,lt.key)}>
+              <div className="flex justify-center cursor-pointer  rounded" onClick={()=>toggleadd(show_day,lt.key)}>
                 <div className="w-1/5">
                   {lt.key == 'lunch' && <Lunch className="w-8" />}
                   {lt.key == 'dinner' && <Dinner className="w-8" />}
@@ -223,23 +224,23 @@ function App() {
             </div>
           );
         })}
+          <div className="flex justify-end">
+            <button className="bg-blue-500 py-1 px-2 mx-1 rounded text-white hover:bg-blue-600" onClick={()=>setShowDay(false)}>Atrás</button>
 
-        <div className="flex justify-end">
-          <button className="bg-blue-500 py-1 px-2 mx-1 rounded text-white hover:bg-blue-600" onClick={()=>setShowDay(false)}>Atrás</button>
-
-        </div>
-      </div>
+          </div>
+        </div> 
+      </div> 
     )
 
   }
   
   if (!currentMenu) return null;
 
-  const day_month = new Date(currentMenu.date.seconds*1000).getDate();
+  const day_month = new Date(currentMenu.date.seconds*1000);
   
 
   return (
-    <div className="p-2 flex flex-col items-center">
+    <div className="p-2 flex flex-col items-center bg-blue-100">
       {isAdding !== false && (
         <div className="fixed inset-0 w-full items-center flex justify-center" style={{backgroundColor:'rgba(0,0,0,0.5)'}}>
           <div className="bg-white w-3/4 p-2 rounded" style={{height: "75vh"}}>
@@ -256,17 +257,17 @@ function App() {
       )}
 
       {currentMenu && week.map( (day,i) => {
-
-
-
+        const day_month = new Date(currentMenu.date.seconds*1000);
+        const week_day = new Date(day_month.setDate(day_month.getDate()+parseInt(i))).getDate();
         return(
-          <div key={i} className="mb-2 flex flex-col w-3/4">
+          <div key={i} className="mb-2 flex flex-row w-3/4 bg-blue-300 rounded shadow-xl border-2 border-blue-500 cursor-pointer" onClick={()=>viewDay(day.key,week_day)}>
 
-            <div className="flex">
-              <MenuPlanner.CalendarDay day={day_month+parseInt(i)} />
-              <h2 className="text-2xl underline ml-4"><a href="" onClick={e=>viewDay(e,day.key)}>{day.day}</a></h2>
+            <div className="flex flex-col items-center justify-center w-12 py-2 ">
+              <span className="text-xs">{day.day.substr(0,3)}</span>
+              <MenuPlanner.CalendarDay day={week_day} />
             </div>
-            <div className="flex justify-between">
+
+            <div className="flex items-center border-l-2 border-gray-300 flex-grow pl-2">
               <div className="">
                 {currentMenu != null && lunch_time.map((lt,i)=>{
                   const meals = currentMenu[day.key][lt.key];
@@ -274,7 +275,7 @@ function App() {
                   if (meals == undefined || meals.length==0) return null;
 
                   return (
-                    <div className="flex">
+                    <div className="flex mb-2 flex-grow">
                       {lt.key == 'lunch' && <Lunch className="w-6" />}
                       {lt.key == 'dinner' && <Dinner className="w-6" />}
                       <div>
@@ -288,9 +289,6 @@ function App() {
                   );
 
                 })}
-              </div>
-              <div>
-                <button className="bg-green-500 py-1 px-2 mx-1 rounded text-white hover:bg-green-600" onClick={()=>viewDay(undefined,day.key)}>Añadir</button>
               </div>
             </div>
           </div>
--- src/components/MenuPlanner.js
+++ src/components/MenuPlanner.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const CalendarDay = ({day}) => {
 
 	return (
-		<div className="border border-black w-8 text-center font-bold text-xl" style={{borderTop:"5px solid red"}}>
+		<div className="border border-black w-8 bg-white text-center font-bold text-xl" style={{borderTop:"5px solid red"}}>
           {day}
         </div>
 	)
